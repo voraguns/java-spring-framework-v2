f6176b8a98e44df28bf8eabf0b96d57c92c1879d
Voragun <voragun2543@gmail.com>
2022-10-11 19:39:21 +0700

    create Annotation by myself

--- code/Start.java
+++ code/Start.java
@@ -1,9 +1,35 @@
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.core.SpringVersion;
 
+
+@Love(city="Bangkok")
 class Start {
+    
+    @ShowLog()
+    
     public static void main(String[] data) {
-        System.out.println(SpringVersion.getVersion());
-        System.out.println("Hello world");
+        ApplicationContext context;
+        context = new AnnotationConfigApplicationContext();
+        User u = new User();
     }
 }
+
+@Deprecated
+class User {
+    int id;
+    @Deprecated String name;
+    String email;
+}
+
+class Member {
+    
+}
+
+// Create Annotation by myself
+@interface Love {
+    String city();
+}
+
+@interface ShowLog { }
