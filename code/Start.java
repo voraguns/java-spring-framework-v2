f1aa4af2ab12d7e5f352a18661db6bd9041d9283
Voragun <voragun2543@gmail.com>
2022-10-11 19:24:11 +0700

    use spring-context and spring-core and get version

---
+++ code/Start.java
@@ -0,0 +1,9 @@
+
+import org.springframework.core.SpringVersion;
+
+class Start {
+    public static void main(String[] data) {
+        System.out.println(SpringVersion.getVersion());
+        System.out.println("Hello world");
+    }
+}
